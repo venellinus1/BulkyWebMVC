c410b7aa3755f2573780106949260978442720f1
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-06-27 13:12:59 +0200

    moving code to the corresponding sub projects

---
+++ Bulky.DataAccess/Migrations/20240623155553_seedCategoryTable.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.DataAccess.Migrations;
+
+/// <inheritdoc />
+public partial class seedCategoryTable : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.RenameColumn(
+            name: "DisplayName",
+            table: "Categories",
+            newName: "DisplayOrder");
+
+        migrationBuilder.InsertData(
+            table: "Categories",
+            columns: new[] { "Id", "DisplayOrder", "Name" },
+            values: new object[,]
+            {
+                { 1, 1, "Action" },
+                { 2, 1, "SciFi" },
+                { 3, 1, "History" }
+            });
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DeleteData(
+            table: "Categories",
+            keyColumn: "Id",
+            keyValue: 1);
+
+        migrationBuilder.DeleteData(
+            table: "Categories",
+            keyColumn: "Id",
+            keyValue: 2);
+
+        migrationBuilder.DeleteData(
+            table: "Categories",
+            keyColumn: "Id",
+            keyValue: 3);
+
+        migrationBuilder.RenameColumn(
+            name: "DisplayOrder",
+            table: "Categories",
+            newName: "DisplayName");
+    }
+}
