fb86471e585e9826f48c23056828511c00da9426
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-07-01 13:38:10 +0200

    Add Product model

---
+++ Bulky.Models/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models.Models;
+
+public class Product
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    public string Description { get; set; }
+    [Required]
+    public string ISBN { get; set; }
+    [Required]
+    public string Author { get; set; }
+
+    [Required]
+    [Display(Name = "Price for 1-50")]
+    [Range(1, 1000)]
+    public double Price { get; set; }
+
+    [Required]
+    [Display(Name = "Price for 50+")]
+    [Range(1, 1000)]
+    public double ListPrice50 { get; set; }
+
+    [Required]
+    [Display(Name = "Price for 100+")]
+    [Range(1, 1000)]
+    public double ListPrice100 { get; set; }
+}
