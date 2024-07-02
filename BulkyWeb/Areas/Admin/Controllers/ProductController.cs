1e41491d388375e2c647b74dcf8f92114124febb
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-07-02 10:22:22 +0200

    Add UI for CRUD operations on Admin Product

---
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,85 @@
+﻿using Bulky.Models;
+using Bulky.Models.Models;
+using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class ProductController(IUnitOfWork unitOfWork)
+    : Controller
+{
+    public IActionResult Index()
+    {
+        var productList = unitOfWork.Product.GetAll().ToList();
+        return View(productList);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Product product)
+    {        
+        if (ModelState.IsValid)
+        {
+            unitOfWork.Product.Add(product);
+            unitOfWork.Save();
+            TempData["success"] = "Product created successfully";
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        Product? productFromDb = unitOfWork.Product.Get(c => c.Id == id);
+
+        if (productFromDb == null)
+            return NotFound();
+
+        return View(productFromDb);
+    }
+    [HttpPost]
+    public IActionResult Edit(Product product)
+    {        
+        if (ModelState.IsValid)
+        {
+            unitOfWork.Product.Update(product);
+            unitOfWork.Save();
+            TempData["success"] = "Product updated successfully";
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        Product? productromDb = unitOfWork.Product.Get(c => c.Id == id);
+
+        if (productromDb == null)
+            return NotFound();
+
+        return View(productromDb);
+    }
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeletePost(int? id)
+    {
+        Product? product = unitOfWork.Product.Get(c => c.Id == id);
+        if (product == null)
+            return NotFound();
+        unitOfWork.Product.Remove(product);
+        unitOfWork.Save();
+        TempData["success"] = "Product deleted successfully";
+        return RedirectToAction("Index");
+    }
+}
