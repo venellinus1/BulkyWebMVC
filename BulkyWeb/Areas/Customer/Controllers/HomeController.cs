8c79e957ef1539ba4e105fe3c44a895c833b9fce
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-07-04 15:15:57 +0200

    HomePage - show details- product image, name, author, listprice and price with rebate

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,23 +1,28 @@
 
 using Bulky.Models;
+using Bulky.Models.Models;
+using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace BulkyWeb.Areas.Customer.Controllers;
 
 [Area("Customer")]
-public class HomeController : Controller
+public class HomeController
+    (ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+    : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    //private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    //public HomeController(ILogger<HomeController> logger)
+    //{
+    //    _logger = logger;
+    //}
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Product> productList = unitOfWork.Product.GetAll(includeProperties: "Category");
+        return View(productList);
     }
 
     public IActionResult Privacy()
