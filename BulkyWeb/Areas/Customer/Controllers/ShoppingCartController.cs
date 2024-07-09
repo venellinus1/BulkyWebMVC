0236c075d43915368b1a352cbb03b132fbb00fdb
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-07-09 16:26:27 +0200

    ShoppingCart - order total, calculate based on selected items count

--- BulkyWeb/Areas/Customer/Controllers/ShoppingCartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -1,4 +1,5 @@
-﻿using Bulky.Models.ViewModels;
+﻿using Bulky.Models.Models;
+using Bulky.Models.ViewModels;
 using BulkyBook.DataAccess.Repository;
 using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Authorization;
@@ -23,6 +24,30 @@ public class ShoppingCartController
         {
             ShoppingCartList = unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product"),
         };
+        foreach (var cart in ShoppingCartVM.ShoppingCartList)
+        {
+            cart.Price = GetPriceBasedOnQuantity(cart);
+            ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
+        }
         return View(ShoppingCartVM);
+    }
+
+    private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
+    {
+        if (shoppingCart.Count <= 50)
+        {
+            return shoppingCart.Product.Price;
+        }
+        else
+        {
+            if (shoppingCart.Count <= 100)
+            {
+                return shoppingCart.Product.Price50;
+            }
+            else
+            {
+                return shoppingCart.Product.Price100;
+            }
+        }
     }
 }
