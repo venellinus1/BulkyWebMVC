49341f505ae27a2cd92fce0413819feec91a3796
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-07-09 15:34:33 +0200

    CartController added Shopping Cart UI  - added from template in ShoppingCart Index

---
+++ BulkyWeb/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Customer.Controllers;
+
+public class ShoppingCartController : Controller
+{
+    [Area("Customer")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
