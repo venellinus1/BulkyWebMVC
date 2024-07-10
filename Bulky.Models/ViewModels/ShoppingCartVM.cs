2d8eea8ab205d49593086b8744415235a18f261c
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-07-10 16:56:45 +0200

    ShoppingCart - Summary Get method ShoppingCartVM - remove OrderTotal, add OrderHeader

--- Bulky.Models/ViewModels/ShoppingCartVM.cs
+++ Bulky.Models/ViewModels/ShoppingCartVM.cs
@@ -10,5 +10,5 @@ namespace Bulky.Models.ViewModels;
 public class ShoppingCartVM
 {
     public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }    
-    public double OrderTotal { get; set; }
+    public OrderHeader OrderHeader { get; set; }
 }
