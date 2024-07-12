c4650663b8c6133a3be29283f23246bc4aed508a
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-07-12 14:22:58 +0200

    OrderManage - OrderDetails get action

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,10 +1,15 @@
 ﻿using Bulky.Models.Models;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
 using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BulkyWeb.Areas.Admin.Controllers;
 
 [Area("admin")]
+[Authorize]
 public class OrderController(IUnitOfWork unitOfWork)
 	: Controller
 {
@@ -12,12 +17,38 @@ public class OrderController(IUnitOfWork unitOfWork)
 	{
 		return View();
 	}
-	#region API CALLS
-	[HttpGet]
-	public IActionResult GetAll()
+    public IActionResult Details(int orderId)
+    {
+        OrderVM orderVM= new()
+        {
+            OrderHeader = unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+            OrderDetail = unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
+        };
+        return View(orderVM);
+    }
+    #region API CALLS
+    [HttpGet]
+	public IActionResult GetAll(string status)
 	{
-		List<OrderHeader> orderHeaderList = unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
-		return Json(new { data = orderHeaderList });
+		IEnumerable<OrderHeader> orderHeaderList = unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+        switch (status)
+        {
+            case "pending":
+                orderHeaderList = orderHeaderList.Where(o => o.PaymentStatus == StaticDetails.PaymentStatusDelayedPayment);
+                break;
+            case "inprocess":
+                orderHeaderList = orderHeaderList.Where(o => o.PaymentStatus == StaticDetails.StatusInProcess);
+                break;
+            case "completed":
+                orderHeaderList = orderHeaderList.Where(o => o.PaymentStatus == StaticDetails.StatusShipped);
+                break;
+            case "approved":
+                orderHeaderList = orderHeaderList.Where(o => o.PaymentStatus == StaticDetails.StatusApproved);
+                break;
+            default:                
+                break;
+        }
+        return Json(new { data = orderHeaderList });
 	}
 
 	#endregion
